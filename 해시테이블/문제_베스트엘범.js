9a38bbf4f02733869b2ba86db318ad073e53c09c
hyunsu.An <ahs39490215@gmail.com>
2023-02-20 23:40:38 +0900

    힙 구현

---
+++ 해시테이블/문제_베스트엘범.js
@@ -0,0 +1,22 @@
+function solution(genres, plays) {
+  const genreMap = new Map();
+
+  genres
+    .map((v, i) => [v, plays[i]])
+    .forEach(([genre, play], index) => {
+      const data = genreMap.get(genre) || { total: 0, songs: [] };
+      genreMap.set(genre, {
+        total: data.total + play,
+        songs: [...data.songs, { play, index }]
+          .sort((a, b) => b.play - a.play)
+          .slice(0, 2),
+      });
+    });
+
+  return [...genreMap.entries()]
+    .sort((a, b) => b[1].total - a[1].total)
+    .flatMap((item) => item[1].songs)
+    .map((song) => song.index);
+}
+
+// Map 객체 개념, flatMap, data가 없을경우를 고려
