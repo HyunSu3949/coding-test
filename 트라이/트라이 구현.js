c9a32f156425ab3dd13088c91f6f8191e202dc1a
hyunsu.An <ahs39490215@gmail.com>
2023-03-05 23:55:48 +0900

    이진탐색

---
+++ 트라이/트라이 구현.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(value = "") {
+    this.value = value;
+    this.children = new Map();
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new Node();
+  }
+
+  insert(string) {
+    let currentNode = this.root;
+
+    for (const char of string) {
+      if (!currentNode.children.has(char)) {
+        currentNode.children.set(char, new Node(currentNode.value + char));
+      }
+      currentNode = currentNode.children.get(char);
+    }
+  }
+
+  has(string) {}
+}
+
+const trie = new Trie();
+
+trie.insert("cat");
+trie.insert("cap");
